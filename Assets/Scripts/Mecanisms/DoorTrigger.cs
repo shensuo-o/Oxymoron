8a5a3f9877fd0d8a3642ccb98eb2c17a20d62e75
Alakyr <manugandara0@gmail.com>
2025-05-04 19:21:57 -0300

    Level diseñado

--- Assets/Scripts/Mecanisms/DoorTrigger.cs
+++ Assets/Scripts/Mecanisms/DoorTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DoorTrigger : MonoBehaviour
 {
     public GameObject door;
+    public GameObject plataform;
     public bool open;
 
     private void Start()
@@ -16,7 +17,8 @@ public class DoorTrigger : MonoBehaviour
     {
         if (other.gameObject.layer == 10 && !open)
         {
-            door.transform.position += new Vector3(0, 5, 0);
+            door.transform.position += new Vector3(0, 11, 0);
+            plataform.transform.position += new Vector3(0, 4, 0);
             open = true;
         }
     }
