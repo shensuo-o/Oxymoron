dd46a75aa615d39b84861fb4eeb385cff9931b78
Inaki Santiago <inakisanti56@gmail.com>
2025-05-12 23:00:53 -0300

    Agregado interactuar con items y prompt de tutorial

--- Assets/Scripts/Leif/ProximityTrigger.cs
+++ Assets/Scripts/Leif/ProximityTrigger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ProximityTrigger : MonoBehaviour
 {
@@ -8,17 +9,21 @@ public class ProximityTrigger : MonoBehaviour
     [SerializeField] private float TriggerDistance;
     [SerializeField] private GameObject prompt;
     [SerializeField] private string Type;
+    [SerializeField] private string Message;
+    [SerializeField] private float timeToRead;
+    [SerializeField] private bool isRead;
 
     void Awake()
     {
         Leif = GameObject.Find("Leif");
+        isRead = false;
     }
 
     void Update()
     {
         var dist = transform.position - Leif.transform.position;
 
-        if (dist.magnitude <= TriggerDistance)
+        if (dist.magnitude <= TriggerDistance && timeToRead >= 0)
         {
             prompt.SetActive(true);
             if (Input.GetKeyDown(KeyCode.E) && Type == "Item")
@@ -27,10 +32,26 @@ public class ProximityTrigger : MonoBehaviour
                 prompt.SetActive(false);
                 this.gameObject.SetActive(false);
             }
+            else if(Type == "Tutorial")
+            {
+                isRead = true;
+                timeToRead -= Time.deltaTime;
+                prompt.gameObject.GetComponent<TextMeshProUGUI>().text = Message;
+            }
         }
         else
         {
+            if (isRead)
+            {
+                timeToRead = 0;
+            }
+
             prompt.SetActive(false);
+
+            if (timeToRead <= 0)
+            {
+                this.gameObject.SetActive(false);
+            }
         }
     }
 }
