a800f2b8a88bbacd66bd039d25430f688f379fd6
Manuel Gandara <manugandara0@gmail.com>
2025-07-07 01:46:10 -0300

    Seteadas animaciones de Leif para que ya no sean Read Only

--- Assets/Scripts/Sonidos/sonido.cs
+++ Assets/Scripts/Sonidos/sonido.cs
@@ -6,8 +6,30 @@ public class sonido : MonoBehaviour
 {
     public AudioSource source;
 
-    public void PlaySound(AudioClip clip)
+    /*public void PlaySound(AudioClip clip)
     {
         source.PlayOneShot(clip);
     }
+
+    public void StopSound()
+    {
+        if (source.isPlaying)
+        {
+            source.Stop();
+        }
+    }*/
+
+    public void PlaySound(AudioClip clip)
+    {
+        source.clip = clip;
+        source.Play();
+    }
+
+    public void StopSound()
+    {
+        if (source.isPlaying)
+            source.Stop();
+    }
+
 }
+
