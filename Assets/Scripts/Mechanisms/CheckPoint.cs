779e89d4737ea3bf936c556787ba9f4abcc7cbe1
Angelo :3 <angelobarchada@gmail.com>
2025-07-11 04:03:08 -0300

    checkpoints finish

--- Assets/Scripts/Mechanisms/CheckPoint.cs
+++ Assets/Scripts/Mechanisms/CheckPoint.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+    private AudioSource source;
+    public AudioClip clips;
+    public ParticleSystem fireflies;
+    public Color newColor;
+    public bool madeSound = false;
+
+    private void Awake()
+    {
+        source = this.GetComponent<AudioSource>();
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 7)
         {
+ 
             SaveSpawn.Instance.transform.position = transform.position;
+            if(!madeSound)
+            {
+                PlaySound(clips);
+                madeSound = true;
+            }
+        
+            Color colorWithAlpha = new Color(newColor.r, newColor.g, newColor.b, 1f);
+            var mainPS = fireflies.main;
+            var noisePS = fireflies.noise;
+            mainPS.startColor = new ParticleSystem.MinMaxGradient(colorWithAlpha);
+            noisePS.strength = 3;
         }
+    }
+
+
+    public void PlaySound(AudioClip clip)
+    {
+        source.clip = clip;
+        source.Play();
     }
 }
